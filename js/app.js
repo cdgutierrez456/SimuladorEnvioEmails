2597de3e10f0323182347166f35704d151118371
Cristian Gutierrez <cdgutierrez456@gmail.com>
2021-06-28 11:07:28 -0500

    Validacion campo email con expresiones regulares

--- js/app.js
+++ js/app.js
@@ -30,21 +30,30 @@ function iniciarApp() {
 // Validando formulario 
 
 function validarFormulario(e) {
-    if(e.target.value.length > 0) {
-        // console.log('bien')
+    if (e.target.value.length > 0) {
+        // Eliminando cuadro de errores
+        const error = document.querySelector('p.error');
+        error.remove();
         e.target.style.borderBottomColor = 'green';
     } else {
         e.target.style.borderBottomColor = 'red';
         mostrarError('Todos los campos son obligatorios');
     }
 
-    if(e.target.type === 'email') {
-        const resultado = e.target.value.indexOf('@');
-        if(resultado < 0) {
-            mostrarError('El email no es valido');
+    if (e.target.type === 'email') {
+
+        const er = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+        if (er.test(e.target.value)) {
+            // Eliminando cuadro de errores
+            const error = document.querySelector('p.error');
+            error.remove();
+            e.target.style.borderBottomColor = 'green';
+        } else {
+            e.target.style.borderBottomColor = 'red';
+            mostrarError('E-mail no valido');
         }
     }
-
 }
 
 function mostrarError(mensaje) {
@@ -53,7 +62,7 @@ function mostrarError(mensaje) {
     mensajeError.classList.add('border', 'border-red-500', 'background-color-100', 'text-red-500', 'p-3', 'mt-2', 'text-center', 'error');
 
     const errores = document.querySelectorAll('.error');
-    if(errores.length === 0) {
+    if (errores.length === 0) {
         formulario.appendChild(mensajeError);
     }
 
